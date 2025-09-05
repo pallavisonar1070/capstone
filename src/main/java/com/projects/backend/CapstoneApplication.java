99759086c46d9f804fd227805a259e947296469c
Pallavi <pallavisonar1070@gmail.com>
2025-09-05 13:38:45 +0530

    initial push

---
+++ src/main/java/com/projects/backend/CapstoneApplication.java
@@ -0,0 +1,14 @@
+package com.projects.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CapstoneApplication.class, args);
+	}
+
+}
