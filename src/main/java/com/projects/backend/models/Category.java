30def48168f4186a6d41b373ca3e98d3b6212ebd
Pallavi <pallavisonar1070@gmail.com>
2025-09-06 18:01:59 +0530

    proxy service

--- src/main/java/com/projects/backend/models/Category.java
+++ src/main/java/com/projects/backend/models/Category.java
@@ -8,4 +8,20 @@ import lombok.Setter;
 public class Category {
     Long id;
     String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
