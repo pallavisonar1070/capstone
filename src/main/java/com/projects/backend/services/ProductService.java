cb229d694813c23170757c986dc5bda3f0ec8bf7
Pallavi <pallavisonar1070@gmail.com>
2025-09-11 23:39:36 +0530

    exception handler

--- src/main/java/com/projects/backend/services/ProductService.java
+++ src/main/java/com/projects/backend/services/ProductService.java
@@ -3,10 +3,11 @@ package com.projects.backend.services;
 import com.projects.backend.models.Product;
 import org.springframework.stereotype.Service;
 
+import javax.management.InstanceNotFoundException;
 import java.util.List;
 
 public interface ProductService {
-    Product getProductById(Long id);
+    Product getProductById(Long id) throws InstanceNotFoundException;
     List<Product> getAllProducts();
 
     Product replaceProduct(long id, Product product);
